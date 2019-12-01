161755881ae35807c2d8d36ab183607d3d1f60d7
chidiebere onyedinma <codebreakersmd@gmail.com>
2019-12-01 12:28:45 -0500

    Odometry Calibration

--- src/main/java/connectivity/Publish.java
+++ src/main/java/connectivity/Publish.java
@@ -1,10 +1,7 @@
 package connectivity;
 
 import DDS.*;
-import MissionData.Telemetry;
-import MissionData.TelemetryDataWriter;
-import MissionData.TelemetryDataWriterHelper;
-import MissionData.TelemetryTypeSupportImpl;
+import MissionData.*;
 import OpenDDS.DCPS.DEFAULT_STATUS_MASK;
 import OpenDDS.DCPS.TheParticipantFactory;
 import OpenDDS.DCPS.TheServiceParticipant;
@@ -14,12 +11,101 @@ public class Publish {
 
     private DomainParticipantFactory domainParticipantFactory;
     private DomainParticipant domainParticipant;
-    private Topic topic;
-    private Publisher publisher;
-    private TelemetryTypeSupportImpl telemetryTypeSupport;
     private DataWriter dataWriter;
-    private DataWriterQos dw_qos;
-    private DataWriterQosHolder qosh;
+    private String dataType;
+    private String topicString;
+    private int domainID;
+    private int durabilityKind;
+    private int historyKind;
+    private int serviceHistoryKind;
+    private int livelinessKind;
+    private int reliabilityKind;
+    private int orderKind;
+    private int ownershipKind;
+
+    public Publish(int domainID, String topicString, String dataType, int durabilityKind, int historyKind,
+                   int serviceHistoryKind, int livelinessKind, int reliabilityKind, int orderKind, int ownershipKind){
+        this.dataType = dataType;
+        this.topicString = topicString;
+        this.domainID = domainID;
+        this.durabilityKind = durabilityKind;
+        this.historyKind = historyKind;
+        this.serviceHistoryKind = serviceHistoryKind;
+        this.livelinessKind = livelinessKind;
+        this.reliabilityKind = reliabilityKind;
+        this.orderKind = orderKind;
+        this.ownershipKind = ownershipKind;
+    }
+
+
+    public static class Builder{
+        private String dataType = "";
+        private String topicString = "topic";
+        private int domainID = 1;
+        private int durabilityKind = 0;
+        private int historyKind = 0;
+        private int serviceHistoryKind = 0;
+        private int livelinessKind = 0;
+        private int reliabilityKind = 0;
+        private int orderKind = 0;
+        private int ownershipKind = 0;
+
+        public Builder(){}
+
+        public Builder setDataType(String dataType) {
+            this.dataType = dataType;
+            return this;
+        }
+
+        public Builder setTopicString(String topicString) {
+            this.topicString = topicString;
+            return this;
+        }
+
+        public Builder setDomainID(int domainID) {
+            this.domainID = domainID;
+            return this;
+        }
+
+        public Builder setDurabilityKind(int durabilityKind) {
+            this.durabilityKind = durabilityKind;
+            return this;
+        }
+
+        public Builder setHistoryKind(int historyKind) {
+            this.historyKind = historyKind;
+            return this;
+        }
+
+        public Builder setServiceHistoryKind(int serviceHistoryKind) {
+            this.serviceHistoryKind = serviceHistoryKind;
+            return this;
+        }
+
+        public Builder setLivelinessKind(int livelinessKind) {
+            this.livelinessKind = livelinessKind;
+            return this;
+        }
+
+        public Builder setReliabilityKind(int reliabilityKind) {
+            this.reliabilityKind = reliabilityKind;
+            return this;
+        }
+
+        public Builder setOrderKind(int orderKind) {
+            this.orderKind = orderKind;
+            return this;
+        }
+
+        public Builder setOwnershipKind(int ownershipKind) {
+            this.ownershipKind = ownershipKind;
+            return this;
+        }
+
+        public Publish build(){
+            return new Publish(domainID, topicString, dataType, durabilityKind, historyKind, serviceHistoryKind, livelinessKind, reliabilityKind, orderKind, ownershipKind);
+        }
+    }
 
 
     /*
@@ -28,6 +114,14 @@ public class Publish {
      */
     public void setUp(String args[]) {
 
+        Topic topic = null;
+        Publisher publisher;
+        AgentActionTypeSupportImpl agentActionTypeSupport;
+        MissionPlanTypeSupportImpl missionPlanTypeSupport;
+        TelemetryTypeSupportImpl telemetryTypeSupport;
+        DataWriterQos dw_qos;
+        DataWriterQosHolder qosh;
+
         domainParticipantFactory = TheParticipantFactory.WithArgs(new StringSeqHolder(args));
         if (domainParticipantFactory == null) {
             System.err.println("ERROR: Domain Participant Factory not found");
@@ -35,26 +129,57 @@ public class Publish {
         }
 
         //Create the domain Participant
-        domainParticipant = domainParticipantFactory.create_participant(4,
+        domainParticipant = domainParticipantFactory.create_participant(this.domainID,
                 PARTICIPANT_QOS_DEFAULT.get(), null, DEFAULT_STATUS_MASK.value);
         if (domainParticipant == null) {
             System.err.println("ERROR: Domain Participant creation failed");
             return;
         }
 
-        //Structure 1 Message
-        telemetryTypeSupport = new TelemetryTypeSupportImpl();
-        if (telemetryTypeSupport.register_type(domainParticipant, "") != RETCODE_OK.value) {
-            System.err.println("ERROR: register_type failed");
-            return;
+        switch (this.dataType){
+            case "AgentAction":
+                agentActionTypeSupport = new AgentActionTypeSupportImpl();
+                if (agentActionTypeSupport.register_type(domainParticipant, "") != RETCODE_OK.value) {
+                    System.err.println("ERROR: register_type failed");
+                    return;
+                }
+
+                topic = domainParticipant.create_topic(this.topicString,
+                        agentActionTypeSupport.get_type_name(),
+                        TOPIC_QOS_DEFAULT.get(),
+                        null,
+                        DEFAULT_STATUS_MASK.value);
+                break;
+            case "MissionPlan":
+                missionPlanTypeSupport = new MissionPlanTypeSupportImpl();
+                if (missionPlanTypeSupport.register_type(domainParticipant, "") != RETCODE_OK.value) {
+                    System.err.println("ERROR: register_type failed");
+                    return;
+                }
+
+                topic = domainParticipant.create_topic(this.topicString,
+                        missionPlanTypeSupport.get_type_name(),
+                        TOPIC_QOS_DEFAULT.get(),
+                        null,
+                        DEFAULT_STATUS_MASK.value);
+                break;
+            case "Telemetry":
+                telemetryTypeSupport = new TelemetryTypeSupportImpl();
+                if (telemetryTypeSupport.register_type(domainParticipant, "") != RETCODE_OK.value) {
+                    System.err.println("ERROR: register_type failed");
+                    return;
+                }
+
+                topic = domainParticipant.create_topic(this.topicString,
+                        telemetryTypeSupport.get_type_name(),
+                        TOPIC_QOS_DEFAULT.get(),
+                        null,
+                        DEFAULT_STATUS_MASK.value);
+                break;
         }
 
-        //Topic1
-        topic = domainParticipant.create_topic("Telemetry data",
-                telemetryTypeSupport.get_type_name(),
-                TOPIC_QOS_DEFAULT.get(),
-                null,
-                DEFAULT_STATUS_MASK.value);
+
+
         if (topic == null) {
             System.err.println("ERROR: Topic creation failed");
             return;
@@ -74,24 +199,24 @@ public class Publish {
 
         dw_qos = new DataWriterQos();
         dw_qos.durability = new DurabilityQosPolicy();
-        dw_qos.durability.kind = DurabilityQosPolicyKind.from_int(0);
+        dw_qos.durability.kind = DurabilityQosPolicyKind.from_int(this.durabilityKind);
         dw_qos.durability_service = new DurabilityServiceQosPolicy();
-        dw_qos.durability_service.history_kind = HistoryQosPolicyKind.from_int(0);
+        dw_qos.durability_service.history_kind = HistoryQosPolicyKind.from_int(this.serviceHistoryKind);
         dw_qos.durability_service.service_cleanup_delay = new Duration_t();
         dw_qos.deadline = new DeadlineQosPolicy();
         dw_qos.deadline.period = new Duration_t();
         dw_qos.latency_budget = new LatencyBudgetQosPolicy();
         dw_qos.latency_budget.duration = new Duration_t();
         dw_qos.liveliness = new LivelinessQosPolicy();
-        dw_qos.liveliness.kind = LivelinessQosPolicyKind.from_int(0);
+        dw_qos.liveliness.kind = LivelinessQosPolicyKind.from_int(this.livelinessKind);
         dw_qos.liveliness.lease_duration = new Duration_t();
         dw_qos.reliability = new ReliabilityQosPolicy();
-        dw_qos.reliability.kind = ReliabilityQosPolicyKind.from_int(0);
+        dw_qos.reliability.kind = ReliabilityQosPolicyKind.from_int(this.reliabilityKind);
         dw_qos.reliability.max_blocking_time = new Duration_t();
         dw_qos.destination_order = new DestinationOrderQosPolicy();
-        dw_qos.destination_order.kind = DestinationOrderQosPolicyKind.from_int(0);
+        dw_qos.destination_order.kind = DestinationOrderQosPolicyKind.from_int(this.orderKind);
         dw_qos.history = new HistoryQosPolicy();
-        dw_qos.history.kind = HistoryQosPolicyKind.from_int(0);
+        dw_qos.history.kind = HistoryQosPolicyKind.from_int(this.historyKind);
         dw_qos.resource_limits = new ResourceLimitsQosPolicy();
         dw_qos.transport_priority = new TransportPriorityQosPolicy();
         dw_qos.lifespan = new LifespanQosPolicy();
@@ -99,7 +224,7 @@ public class Publish {
         dw_qos.user_data = new UserDataQosPolicy();
         dw_qos.user_data.value = new byte[0];
         dw_qos.ownership = new OwnershipQosPolicy();
-        dw_qos.ownership.kind = OwnershipQosPolicyKind.from_int(0);
+        dw_qos.ownership.kind = OwnershipQosPolicyKind.from_int(this.ownershipKind);
         dw_qos.ownership_strength = new OwnershipStrengthQosPolicy();
         dw_qos.writer_data_lifecycle = new WriterDataLifecycleQosPolicy();
 
@@ -150,25 +275,53 @@ public class Publish {
         ws.detach_condition(sc);
     }
 
-    public void write(String txtData) {
-
-        TelemetryDataWriter personDataWriter = TelemetryDataWriterHelper.narrow(dataWriter);
-        Telemetry telemetry = new Telemetry();
-        telemetry.from = txtData;
-        telemetry.telemetryId = 99;
-        telemetry.telemetry_data = new String[0];
-        String[] data = {"12.34", "13.76"};
-        telemetry.telemetry_data = data;
-
+    public void write(String from, String[] data, int id) {
 
         try {
-            int handle = personDataWriter.register_instance(telemetry);
+
+            int handle = -1;
             int ret = RETCODE_TIMEOUT.value;
-            System.out.println("Inside write block");
+
+            switch (this.dataType){
+                case "AgentAction":
+                    AgentActionDataWriter agentActionDataWriter = AgentActionDataWriterHelper.narrow(dataWriter);
+                    AgentAction agentAction = new AgentAction();
+                    agentAction.from = from;
+                    agentAction.actionId = id;
+                    agentAction.action_data = new String[0];
+                    agentAction.action_data = data;
+                    handle = agentActionDataWriter.register_instance(agentAction);
+                    //write the data
+                    ret = agentActionDataWriter.write(agentAction, handle);
+                    break;
+                case "MissionPlan":
+                    MissionPlanDataWriter missionPlanDataWriter = MissionPlanDataWriterHelper.narrow(dataWriter);
+                    MissionPlan missionPlan = new MissionPlan();
+                    missionPlan.from = from;
+                    missionPlan.planId = id;
+                    missionPlan.plan_data = data;
+                    handle = missionPlanDataWriter.register_instance(missionPlan);
+                    //write the data
+                    ret = missionPlanDataWriter.write(missionPlan, handle);
+
+                    break;
+                case "Telemetry":
+                    TelemetryDataWriter telemetryDataWriter = TelemetryDataWriterHelper.narrow(dataWriter);
+                    Telemetry telemetry = new Telemetry();
+                    telemetry.from = from;
+                    telemetry.telemetryId = id;
+                    telemetry.telemetry_data = data;
+                    handle = telemetryDataWriter.register_instance(telemetry);
+                    //write the data
+                    ret = telemetryDataWriter.write(telemetry, handle);
+
+                    break;
+            }
 
 
-            //write the data
-            ret = personDataWriter.write(telemetry, handle);
+            //agentAction.action_data = new String[0];
+            //String[] data = {"0,0", "1,0", "2,0", "3,0", "4,0", "4,-1", "4,-2", "4,-3", "4,-4"};
+
 
             if (ret != RETCODE_OK.value) {
                 System.err.println("ERROR " +
